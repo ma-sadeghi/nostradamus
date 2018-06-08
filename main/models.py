1cff884001cb01ceead9051610bc071666e051d1
Amin Sadeghi <amin.sadeghi@live.com>
2018-06-08 19:32:09 -0400

    Overhaul of models.

--- main/models.py
+++ main/models.py
@@ -12,11 +12,6 @@ class Team(models.Model):
 
 
 class Game(models.Model):
-    # contest = models.ForeignKey(Contest)
-    # home = models.CharField(max_length=100)
-    # away = models.CharField(max_length=100)
-    # home_abbreviation = models.CharField(max_length=3)
-    # away_abbreviation = models.CharField(max_length=3)
     contest = models.ForeignKey(Contest)
     home = models.ForeignKey(Team)
     away = models.ForeignKey(Team)
@@ -38,6 +33,17 @@ class Bet(models.Model):
         return self.user.username + ': ' + self.game.home + ' - ' + self.game.away
 
 
-class Contest(models.Model):
+class Tournament(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+
+class Contest(models.Model):
+    tournament = models.ForeignKey(Tournament)
+    name = models.CharField(max_length=20)
+    
+
+    def __str__(self):
+        return self.name
