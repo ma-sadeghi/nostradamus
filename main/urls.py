bb97d66b6183f4393310d1ad18d86cc680976346
Amin Sadeghi <amin.sadeghi@live.com>
2018-06-09 19:00:10 -0400

    Continuing the overhaul...

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
-    url(r'^login/$', views.login_view, name='login'),
+    url(r'^$', views.index, name='index'),
+    url(r'^home/$', views.home_view, name='home'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^signup/$', views.SignupView.as_view(), name='signup'),
     url(r'^place_bet/$', views.place_bet, name='place_bet'),
     url(r'^standing/$', views.show_standing, name='standing'),
     url(r'^bets/$', views.show_bets, name='bets'),
+	url(r'^contest/(?P<contest>\w+)/$', views.contest_view, name='contest')
 ]
