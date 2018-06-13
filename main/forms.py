08967dd99427836a8667cc8fc48105508937df9f
Amin Sadeghi <amin.sadeghi@live.com>
2018-06-13 17:19:12 -0400

    Removed crispy_forms imports (no longer used)

--- main/forms.py
+++ main/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.admin import widgets
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
-from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
 
 class LoginForm(forms.Form):
