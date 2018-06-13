0370d2ffe865830b8927ad91736d1e78fa74606b
Amin Sadeghi <amin.sadeghi@live.com>
2018-06-13 17:12:14 -0400

    Added tinyflask.com to ALLOWED_HOSTS

--- Nostradamus/settings.py
+++ Nostradamus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***SCRUBBED-SECRET-KEY***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.tinyflask.com']
 
 LOGIN_URL = '/accounts/login/'
 
