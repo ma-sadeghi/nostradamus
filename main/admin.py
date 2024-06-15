58272047226aeaa0daaac587f020a972f9f9ebfa
Amin Sadeghi <amin.sadeghi@live.com>
2024-06-15 01:41:24 -0400

    Allow importing Games in batch mode from file

--- main/admin.py
+++ main/admin.py
@@ -1,12 +1,26 @@
-import inspect
-
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
-from .models import Bet, Contest, Game, Profile, Team, Tournament
+from .models import Bet, Contest, Game, GameResource, Profile, Team, Tournament
+
+
+class GameAdmin(ImportExportModelAdmin):
+    resource_class = GameResource
+    # Optional: Specify list_display to improve the admin interface usability
+    list_display = (
+        "tournament",
+        "home",
+        "away",
+        "home_score",
+        "away_score",
+        "scheduled_datetime",
+        "isplayoff",
+    )
+
 
 admin.site.register(Profile)
 admin.site.register(Tournament)
 admin.site.register(Contest)
 admin.site.register(Team)
 admin.site.register(Bet)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
