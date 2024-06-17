62196b419d35a12796ec30ea9b290c4f7351d557
Amin Sadeghi <amin.sadeghi@live.com>
2024-06-17 12:12:21 -0400

    Invalidate cache every 24h

--- main/views.py
+++ main/views.py
@@ -145,7 +145,7 @@ def standing_home_view(request):
     return render(request, "standing_home.html", {"contests": contests})
 
 
-@cache_page(7 * 24 * 3600)  # Reset cache every 1 week
+@cache_page(24 * 3600)  # Reset cache every 1 week
 @login_required
 def show_standing(request, contest):
     contest = request.user.contests.filter(name=contest).all()[0]
