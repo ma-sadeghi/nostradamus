b19f14d69e7a383a0b37552a6ae609714706f2ac
Amin Sadeghi <amin.sadeghi@live.com>
2024-06-19 23:17:36 -0400

    chore: Enable explicit handling of static files for deployment via whitenoise

--- Nostradamus/urls.py
+++ Nostradamus/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r"^", include("main.urls")),  # Assuming your main.urls uses regex.
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 handler404 = "main.views.custom_404_view"
--- Nostradamus/wsgi.py
+++ Nostradamus/wsgi.py
@@ -12,7 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
+from .settings import STATIC_ROOT
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Nostradamus.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+application = WhiteNoise(application, root=STATIC_ROOT)
